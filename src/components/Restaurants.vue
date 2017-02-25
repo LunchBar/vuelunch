bf4365856f3fc84edf87983f8abb8b601faaa2e9
Shung-Hsi Yu <shunghsiyu@gmail.com>
2017-02-25 09:52:36 +0800

    Add Restaurants page
    * Add a route to the Restaurants Page.

---
+++ src/components/Restaurants.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="restaurants">
+    <h1>Restaurants</h1>
+    <ul>
+      <li v-for="restaurant in restaurants" >
+          <span v-text="restaurant.name"></span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import api from '../api'
+
+export default {
+  name: 'restaurants',
+  created () {
+    this.fetchData()
+  },
+  data () {
+    return {
+      restaurants: []
+    }
+  },
+  methods: {
+    fetchData () {
+      api
+        .then(client => client.restaurants.restaurants_list())
+        .then(respond => JSON.parse(respond.data))
+        .then(data => data.results)
+        .then(list => {
+          this.restaurants = list
+        })
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.restaurants {
+  li {
+    list-style: none;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Landing from 'components/Landing'
+import Restaurants from 'components/Restaurants'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Landing',
       component: Landing
+    },
+    {
+      path: '/restaurants',
+      name: 'Restaurants',
+      component: Restaurants
     }
   ]
 })
